cb0976eec43b874a91cda06e9b10d5bba3c1848b
eduardo-hernandez-ueno <149789338+eduardo-hernandez-ueno@users.noreply.github.com>
2025-02-11 22:52:15 -0300

    remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import WordleClone from "./WordleClone.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <WordleClone />
-  </StrictMode>,
 )
